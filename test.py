2f0f46c16f4976f78d6d3a06c3079e02326904c6
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2021-10-22 23:14:52 +0300

    game test

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-a = "-0x7fffffd9ad047306"
-print(a.decode("utf-8"))
+a = float(input())
+b = float(input())
+s = input()
+if s == "/" and b == 0 or (s != "+" and s != "-" and s != "*" and s != "/"):
+    print(888888)
+if s == "+":
+    print(a + b)
+if s == "-":
+    print(a - b)
+if s == "*":
+    print(a * b)
+if s == "/":
+    print(a / b)
