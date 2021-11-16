e6ee04e5d3cd14b76619733c095782b358b952f6
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2021-11-16 19:27:55 +0300

    auth and updating data on android + threading

--- main.py
+++ main.py
@@ -40,21 +40,89 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.card import MDCard
 from kivymd.uix.tab import MDTabsBase
 import random
-from decimal import Decimal
+
 #import numpy as np
-from kivy.storage.jsonstore import JsonStore
+
 main_font_size = 20
 import os
 import firebase_admin
 from firebase_admin import db
 import random
-from decimal import Decimal
 
 from threading import Thread
 
-import asyncio
+cred_obj = firebase_admin.credentials.Certificate('ton-clicker-firebase-adminsdk-cf1xz-8ad3090323.json')
+app_d = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': "https://ton-clicker-default-rtdb.firebaseio.com/"
+})
+
+up_data = True
+auth_succefull = False
 class SettingsTab(MDCard, MDTabsBase):
     pass
+
+
+class Auth(Screen):
+    def __init__(self, **kwargs):
+
+        #self.f1 = Widget()
+        super().__init__(**kwargs)
+        self.game = Clicker
+
+        #self.main_font_size = main_font_size
+    def login(self):
+
+        player_email = self.ids["email_l"].text
+        player_password = self.ids["password_l"].text
+
+
+        ref = db.reference(f"/{player_email}")
+        if ref.get()["account"]["password"] == player_password:
+
+            self.player_data = ref.get()["data"]
+            self.account = ref.get()["account"]
+
+
+
+
+
+        self.game.account = self.account
+        self.game.player_data = self.player_data
+        self.game.bot_data = self.player_data["bot"]
+        self.game.summation_data = self.player_data["summation"]
+        self.game.ref = ref
+        self.start_loops()
+    def registration(self):
+        player_name = self.ids["name_r"].text
+        player_email = self.ids["email_r"].text
+        player_password = self.ids["password_r"].text
+        self.data = {
+                            "account": {"name": player_name,
+                                "email": player_email,
+                                "password": player_password},
+                            "data": {"TON": 0, "doubling": 1, "doubling_price": 0.001,
+                                "bot": {"alow_bot": False, "doubling": 1, "doubling_price": 0.001,
+                                    "bot_speed": 0, "bot_price": 1,
+                                    "summation_price": 0.000001, "summation_num": 0.000001},
+
+                                "summation": {"summation_price": 0.000001, "summation_num": 0.000001}
+                                     }
+                            }
+        ref = db.reference(f"/{player_email}")
+        ref.set(self.data)
+        self.player_data = self.data["data"]
+        self.account = self.data["account"]
+
+        self.game.account = self.account
+        self.game.player_data = self.player_data
+        self.game.bot_data = self.player_data["bot"]
+        self.game.summation_data = self.player_data["summation"]
+        self.game.ref = ref
+        self.start_loops()
+    def start_loops(self):
+        global auth_succefull
+        auth_succefull = True
+        self.manager.current = "clicker"
 class Settings_gui(Screen):
 
     def __init__(self, **kwargs):
@@ -68,7 +136,8 @@ class Settings_gui(Screen):
         #Clicker().main_font_size = self.ids["settings_font_slider"]
         #print(Clicker().main_font_size)
     def back(self):
-        self.manager.current = "1"
+        self.manager.current = "clicker"
+
 class ScreenManagement(ScreenManager):
     def __init__(self, **kwargs):
 
@@ -86,12 +155,13 @@ class Clicker(Screen):
             # self.bitcoin = 0
             #self.bitcoin = 0
     #self.size_hint = (1,1)
-        self.loop = asyncio.new_event_loop()
+
         self.n = 0
     def show_value(self):
-        b = f"{self.ids['bet_value'].value}"
+        b = self.ids['bet_value'].value
 
         #m = self.player_data["TON"] - s
+
         w = self.player_data["TON"]/100*b
         self.ids['value_bet_text'].text = f"Ваша ставка: {'{0:.6f}'.format(w)} TON"
     def find_it(self):
@@ -99,14 +169,14 @@ class Clicker(Screen):
 
         r = random.randint(0,100)
 
-        b= f"{self.ids['bet_value'].value}"
-        l = self.ids['bet_value'].value * (-1) + self.ids['bet_value'].max
+        b= self.ids['bet_value'].value
+        l = b * (-1) + self.ids['bet_value'].max
         #m = self.player_data["TON"] - s
         w = self.player_data["TON"]/100*b
         self.player_data["TON"] -= w
 
         #print('{0:.6f}'.format(self.player_data["TON"]), '{0:.7f}'.format(s / 100 * l))
-        if l >= r:
+        if r >= l:
             self.player_data["TON"] += w*2
             self.show_alert_dialog(text=f"Вы выиграли {'{0:.6f}'.format(w*2)} TON")
 
@@ -214,17 +284,17 @@ class Clicker(Screen):
     def to_settings(self):
         #print(self.manager.current)
         self.manager.current = "2"
-    def test_threading(self):
-        self.update_data()
+
     def main_loop(self,dt):
         #self.test_threading()
-        th = Thread(target=self.test_threading)
-        th.start()
+        if up_data:
+            th = Thread(target=self.update_data)
+            th.start()
         #print(self.main_font_size)
         #print('{1000:.9f}'.format(self.summation_data["summation_num"]))
         #print('{0:.7f}'.format(self.summation_data["summation_num"]))
         #print(123)
-        self.ids['show_info'].on_press = lambda: self.show_alert_dialog(text=f'Клик: {self.summation_data["summation_num"]} TON\nУдвоение майнинга: x{self.player_data["doubling"]}\nБот: {self.bot_data["alow_bot"]}\nУдвоение майнинга бота: x{self.bot_data["doubling"]}')
+        self.ids['show_info'].on_press = lambda: self.show_alert_dialog(text=f'''Клик: {'{0:.6f}'.format(self.summation_data["summation_num"])} TON\nУдвоение майнинга: x{self.player_data["doubling"]}\nБот: {self.bot_data["alow_bot"]}\nУдвоение майнинга бота: x{self.bot_data["doubling"]}''')
         #self.ids['settings'].on_press =  self.swi
         self.ids[
             'text_doubling'].text = f'''Удвоение майнинга с:{self.player_data["doubling"] } на 30%\nЦена: {'{0:.6f}'.format(self.player_data["doubling_price"])} TON'''
@@ -251,59 +321,28 @@ class Clicker(Screen):
 
             self.player_data["TON"] += self.bot_data["bot_speed"]*self.player_data["doubling"]+self.summation_data["summation_num"]
 class app(MDApp):
+    def __init__(self, **kwargs):
 
+        #self.f1 = Widget()
+        super().__init__(**kwargs)
+        #self.main_font_size = main_font_size
+        self.auth_succefull = False
+            # self.bitcoin = 0
+            #self.bitcoin = 0
+    #self.size_hint = (1,1)
 
+    def start_loops(self,dt):
+        global auth_succefull
+        if auth_succefull:
+
+            Clock.schedule_interval(self.game.main_loop,1/60)
+            Clock.schedule_interval(self.game.bot_loop, 1)
+
+            auth_succefull = False
     def build(self):
 
-        cred_obj = firebase_admin.credentials.Certificate('ton-clicker-firebase-adminsdk-cf1xz-8ad3090323.json')
-        app = firebase_admin.initialize_app(cred_obj, {
-            'databaseURL': "https://ton-clicker-default-rtdb.firebaseio.com/"
-        })
-
-        print(123)
-        type = input("r/l: ")
-        if type == "r":
-            player_name = input("Имя: ")
-            player_email = input("Электронная почта: ")
-            player_password = input("Пароль: ")
-            self.data = {
-                                "account": {"name": player_name,
-                                    "email": player_email,
-                                    "password": player_password},
-                                "data": {"TON": 0, "doubling": 1, "doubling_price": 0.001,
-                                    "bot": {"alow_bot": False, "doubling": 1, "doubling_price": 0.001,
-                                        "bot_speed": 0, "bot_price": 1,
-                                        "summation_price": 0.000001, "summation_num": 0.000001},
-
-                                    "summation": {"summation_price": 0.000001, "summation_num": 0.000001}
-                                         }
-                                }
-            ref = db.reference(f"/{player_email}")
-            ref.set(self.data)
-            self.player_data = self.data["data"]
-            self.account = self.data["account"]
-        if type == "l":
-
-            player_email = input("Электронная почта: ")
-            player_password = input("Пароль: ")
-
-            ref = db.reference(f"/{player_email}")
-
-            if ref.get()["account"]["password"] == player_password:
-                self.player_data = ref.get()["data"]
-                self.account = ref.get()["account"]
-
-
-
-
-
-
-        game = Clicker(name="1")
-        game.account = self.account
-        game.player_data = self.player_data
-        game.bot_data = self.player_data["bot"]
-        game.summation_data = self.player_data["summation"]
-        game.ref = ref
+        Clock.schedule_interval(self.start_loops, 1/30)
+        self.game = Clicker(name="clicker")
 
         #self.main_font_size = self.settings["font_size"]
         #self.title = "Tap-Fight"
@@ -311,18 +350,22 @@ class app(MDApp):
         #self.theme_cls.primary_palette = "BlueGray"
 
 
-        Clock.schedule_interval(game.main_loop,1/60)
-        Clock.schedule_interval(game.bot_loop, 1)
+
         screen_manager = ScreenManager()
 
         # Add the screens to the manager and then supply a name
         # that is used to switch screens
-        screen_manager.add_widget(game)
+        screen_manager.add_widget(self.game)
 
-        d = Settings_gui(name="2")
-        print(screen_manager.current)
+        d = Settings_gui(name="settings")
+
         #d.folder_path = folder_path
         screen_manager.add_widget(d)
+        self.auth = Auth(name="auth")
+
+        #d.folder_path = folder_path
+        screen_manager.add_widget(self.auth)
+        screen_manager.current = "auth"
 
     #self.background_color=(1,0.1,0.1)
         return screen_manager
@@ -330,3 +373,4 @@ class app(MDApp):
 # Запуск проекта
 if __name__ == "__main__":
     app().run()
+up_data = False
--- t.py
+++ t.py
@@ -1,12 +1,52 @@
-import os
-import urllib.parse as up
-import psycopg2
+'''
+Widget animation
+================
 
-up.uses_netloc.append("postgres")
-url = up.urlparse(os.environ["postgres://hzudggdn:wDvu8befix1OG75oDQ4UV-BIZjhlQzfn@fanny.db.elephantsql.com/hzudggdn"])
-conn = psycopg2.connect(database=url.path[1:],
-user=url.username,
-password=url.password,
-host=url.hostname,
-port=url.port
-)
+This example demonstrates creating and applying a multi-part animation to
+a button widget. You should see a button labelled 'plop' that will move with
+an animation when clicked.
+'''
+
+import kivy
+kivy.require('1.0.7')
+
+from kivy.animation import Animation
+from kivy.app import App
+from kivy.uix.button import Button
+import firebase_admin
+from firebase_admin import db
+
+
+from threading import Thread
+
+cred_obj = firebase_admin.credentials.Certificate('ton-clicker-firebase-adminsdk-cf1xz-8ad3090323.json')
+app = firebase_admin.initialize_app(cred_obj, {
+    'databaseURL': "https://ton-clicker-default-rtdb.firebaseio.com/"
+})
+ref = db.reference("/er")
+print(ref.get())
+class TestApp(App):
+
+    def animate(self, instance):
+        # create an animation object. This object could be stored
+        # and reused each call or reused across different widgets.
+        # += is a sequential step, while &= is in parallel
+        animation = Animation(pos=(100, 100), t='out_bounce')
+        animation += Animation(pos=(200, 100), t='out_bounce')
+        animation &= Animation(size=(500, 500))
+        animation += Animation(size=(100, 50))
+
+        # apply the animation on the button, passed in the "instance" argument
+        # Notice that default 'click' animation (changing the button
+        # color while the mouse is down) is unchanged.
+        animation.start(instance)
+
+    def build(self):
+        # create a button, and  attach animate() method as a on_press handler
+        button = Button(size_hint=(None, None), text='plop',
+                        on_press=self.animate)
+        return button
+
+
+if __name__ == '__main__':
+    TestApp().run()
