3f524436152564300d4c97609d7d046e741ffa77
Arsbul-hub <63012106+Arsbul-hub@users.noreply.github.com>
2021-12-02 16:03:37 +0300

    0.9 + fixed bugs with offline mining and ping test

--- main.py
+++ main.py
@@ -67,29 +67,6 @@ class Error_show(Screen):
         #self.f1 = Widget()
         super().__init__(**kwargs)
 
-    def show_dialog(self, text):
-        self.dialog = None
-        if not self.dialog:
-            self.dialog = MDDialog(
-                text=text,
-                #                text_color=(0,0,0,1),
-                buttons=[
-                    MDFlatButton(
-                        text="Ок",
-                        theme_text_color="Custom",
-                        # text_font_name= "main_font.ttf",
-                        text_color=(0, 0, 0, 1),
-                        font_size=19,
-                        font_name="main_font.ttf",
-                        # text_color=self.theme_cls.primary_color,
-                        on_press=lambda x: self.close_dialog()
-                    ),
-                ],
-            )
-        self.dialog.open()
-
-    def close_dialog(self):
-        self.dialog.dismiss()
     def try_offline(self):
         global auth_succefull, offline, data
         try:
@@ -102,10 +79,7 @@ class Error_show(Screen):
                 self.manager.current = "clicker"
 
         except:
-            self.show_dialog(text='''
-Вы не вошли в свой аккаунт!
-Подключитесь к интернету и войдите в систему.            
-''')
+
             offline = False
 class Auth(Screen):
     def __init__(self, **kwargs):
@@ -115,7 +89,28 @@ class Auth(Screen):
         self.game = Clicker()
 
         #self.main_font_size = main_font_size
-
+    def show_dialog(self,text):
+        self.dialog = None
+        if not self.dialog:
+            self.dialog = MDDialog(
+                text=text,
+#                text_color=(0,0,0,1),
+                buttons=[
+                    MDFlatButton(
+                        text="Ок",
+                        theme_text_color="Custom",
+                        #text_font_name= "main_font.ttf",
+                        text_color=(0,0,0,1),
+                        font_size=19,
+                        font_name="main_font.ttf",
+                        #text_color=self.theme_cls.primary_color,
+                        on_press= lambda x: self.close_dialog()
+                    ),
+                ],
+            )
+        self.dialog.open()
+    def close_dialog(self):
+        self.dialog.dismiss()
 
     def login(self):
         global data
@@ -405,7 +400,8 @@ class Clicker(Screen):
             self.player_data = data["data"]
             self.bot_data = data["data"]["bot"]
             self.summation_data = data["data"]["summation"]
-
+            th = Thread(target=self.update_data)
+            th.start()
 
 
             #print(self.account)
@@ -453,13 +449,6 @@ class Clicker(Screen):
             if self.bot_data["alow_bot"]:
 
                 self.player_data["TON"] += self.bot_data["bot_speed"]*self.player_data["doubling"]+self.summation_data["summation_num"]
-    def theard_update_data(self, dt):
-        global auth_succefull
-        if auth_succefull:
-            th = Thread(target=self.update_data)
-            th.start()
-
-
 class app(MDApp):
 
     def build(self):
@@ -479,7 +468,6 @@ class app(MDApp):
         screen_manager.add_widget(self.game)
         Clock.schedule_interval(self.game.main_loop, 1 / 60)
         Clock.schedule_interval(self.game.bot_loop, 1)
-        Clock.schedule_interval(self.game.theard_update_data, 4)
         d = Settings_gui(name="settings")
 
         #d.folder_path = folder_path
--- t.py
+++ t.py
@@ -1,47 +1,6 @@
-class MainWnd(QMainWindow, mainGui.Ui_RecistMainWindow):
-   def __init__(self, parent=None):
-        super(MainWnd, self).__init__(parent)
-        self.setupUi(self)
-
-        self.actionExit.triggered.connect(self.exitApp)
-        self.actionNew_subject.triggered.connect(self.newsubj_clicked)
-        self.baseline_btn.clicked.connect(self.baseline_clicked)
-
-    def newsubj_clicked(self, parent=None):
-        dialog = NewSubject()
-        dialog.exec_()
-        self.id_label.setText(dialog.createid_lineedit.text())
-        self.baseline_btn.setEnabled(True)
-
-    def baseline_clicked(self):
-        dialogbs = BaseLine(self.id_label.text())
-        dialogbs.exec_()
-
-    def exitApp(self):
-        sys.exit(0)
+from pythonping import ping
 
 
-class NewSubject(QDialog, newidGui.Ui_newSubjId):
-    def __init__(self, parent=None, idparam=None):
-        super(NewSubject, self).__init__(parent)
-        self.setupUi(self)
-        self.createid_lineedit.setFocus()
+d = ping('8.8.8.8',count=1,timeout=1)
 
-
-class BaseLine(QDialog, baselineGui.Ui_BaseLine):
-    def __init__(self, id_value, parent=None):
-        super(BaseLine, self).__init__(parent)
-        self.setupUi(self)
-
-        print(id_value)
-
-
-def main():
-    app = QApplication(sys.argv)
-    form = MainWnd()
-    form.show()
-    app.exec_()
-
-
-if __name__ == "__main__":
-    main()
+print(d.rtt_avg_ms)
